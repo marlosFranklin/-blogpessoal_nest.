d1f7fc0f076272fe4c24c57af8c8098460871115
marlosFranklin <marlos-gama@hotmail.com>
2026-04-01 11:26:10 -0300

    feature:cria os metodos para buscar item por id, buscar todos os itens e deletar item.

---
+++ src/postagem/service/postagem.service.ts
@@ -0,0 +1,37 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Postagem } from '../entities/postagem.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult } from 'typeorm/browser';
+
+@Injectable()
+export class PostagemService {
+  constructor(
+    @InjectRepository(Postagem)
+    private postagemRepository: Repository<Postagem>,
+  ) {}
+
+  async findAll(): Promise<Postagem[]> {
+    return await this.postagemRepository.find();
+  }
+
+  async findById(id: number): Promise<Postagem> {
+    const postagem = await this.postagemRepository.findOne({
+      where: {
+        id,
+      },
+    });
+    if (!postagem)
+      throw new HttpException('Postagem não encontrada ', HttpStatus.NOT_FOUND);
+    return postagem;
+  }
+
+  async create(postagem: Postagem): Promise<Postagem> {
+    return await this.postagemRepository.save(postagem);
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    await this.findById(id);
+    return await this.postagemRepository.delete(id);
+  }
+}
