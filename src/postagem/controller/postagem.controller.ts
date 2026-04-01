246036e356478808acc83db9a62cb9d4c1962eac
marlosFranklin <marlos-gama@hotmail.com>
2026-04-01 11:27:54 -0300

    feat :aad  os metdos buscarporid, buscar todos e deletar, para terem acesso ao endpotins e banco de dados

--- src/postagem/controller/postagem.controller.ts
+++ src/postagem/controller/postagem.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Body,
+  Delete,
+} from '@nestjs/common';
 import { Postagem } from '../entities/postagem.entity';
-import { PostagemService } from '../service/postagem.servce';
+import { PostagemService } from '../service/postagem.service';
 
 @Controller('/postagens')
 export class PostagemController {
@@ -9,5 +19,21 @@ export class PostagemController {
   @HttpCode(HttpStatus.OK)
   findAll(): Promise<Postagem[]> {
     return this.postagemService.findAll();
+  }
+
+  @Get('/:id')
+  findById(@Param('id', ParseIntPipe) id: number) {
+    return this.postagemService.findById(id);
+  }
+
+  @Post('')
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() postagem: Postagem): Promise<Postagem> {
+    return this.postagemService.create(postagem);
+  }
+
+  @Delete('/:id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.postagemService.delete(id);
   }
 }
