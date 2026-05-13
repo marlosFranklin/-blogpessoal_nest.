83ef0ae75a0c76cc607d66552db4124462226f88
marlosFranklin <marlos-gama@hotmail.com>
2026-05-13 11:58:58 -0300

    fix:ambiente de prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,13 @@ import { TemaModule } from './tema/tema.module';
 import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { ConfigModule } from '@nestjs/config';
-import { DevService } from './data/services/dev.service';
+import { ProdService } from './data/services/prod.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
-      useClass: DevService,
+      useClass: ProdService,
       imports: [ConfigModule],
     }),
 
